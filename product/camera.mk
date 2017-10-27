d492b9994cb27d3c12f7a61458af045a3d683599
Jar <jara2105@mail.ru>
2017-10-27 20:58:04 +0500

    Added CameraNext

--- product/camera.mk
+++ product/camera.mk
@@ -5,7 +5,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml
 
-# Next
+# CameraNext
 PRODUCT_PACKAGES += \
     libshims_camera \
     libshim_ims_camera \
