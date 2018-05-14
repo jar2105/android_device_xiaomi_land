8d505d2fca1f22bbe15c4b9bcd3f7e1c9e044c0e
jar2105 <jara2105@mail.ru>
2018-05-14 15:54:57 +0000

    land: rootdir: Remove useless commands

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -49,23 +49,6 @@ function configure_memory_parameters() {
         mkswap /dev/block/zram0
         swapon /dev/block/zram0 -p 32758
     fi
-
-    SWAP_ENABLE_THRESHOLD=1048576
-    swap_enable=`getprop ro.vendor.qti.config.swap`
-
-    # Enable swap initially only for 1 GB targets
-    if [ "$MemTotal" -le "$SWAP_ENABLE_THRESHOLD" ] && [ "$swap_enable" == "true" ]; then
-        # Static swiftness
-        echo 1 > /proc/sys/vm/swap_ratio_enable
-        echo 70 > /proc/sys/vm/swap_ratio
-
-        # Swap disk - 200MB size
-        if [ ! -f /data/system/swap/swapfile ]; then
-            dd if=/dev/zero of=/data/system/swap/swapfile bs=1m count=200
-        fi
-        mkswap /data/system/swap/swapfile
-        swapon /data/system/swap/swapfile -p 32758
-    fi
 }
 
 # Start Host based Touch processing
